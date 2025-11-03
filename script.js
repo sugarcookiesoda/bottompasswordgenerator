5e06f9f461d5da33a153950071c6b28c7f44a5ec
vivian gee <123298169+sugarcookiesoda@users.noreply.github.com>
2025-11-03 09:05:06 -0800

    fixed bug
    (mr. bug)

--- script.js
+++ script.js
@@ -5,10 +5,10 @@ var emoticon = [' >_<', ' @_@', ' >~<']
 
 function generateUsername() 
 {
-    var smash1 = adjectives[Math.floor(Math.random() * adjectives.length)];
-    var smash2 = nouns[Math.floor(Math.random() * nouns.length)];
-    var text = numbers[Math.floor(Math.random() * numbers.length)];
-    var emoticon = numbers[Math.floor(Math.random() * numbers.length)];
+    var smash12 = smash1[Math.floor(Math.random() * smash1.length)];
+    var smash22 = smash2[Math.floor(Math.random() * smash2.length)];
+    var text2 = text[Math.floor(Math.random() * text.length)];
+    var emoticon2 = emoticon[Math.floor(Math.random() * emoticon.length)];
     var username = smash1 + smash2 + text + emoticon;
 
  document.getElementById('username').textContent = 'your password is: ' + username;
